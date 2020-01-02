3015e82c1ecfaf809df4d4ce865af0087bb7da62
Akash Ashok <akashashok@Akashs-MacBook-Pro.local>
2020-01-01 20:21:19 -0800

    Added data to weather feedback

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,8 +15,9 @@ const forecast = (latitude, longitude, callback) => {
             callback('Poorly formatted input. Try again!', undefined)
 
         else {
+            // console.log(body.daily.data[0])
             callback(undefined, 
-                body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain'
+                body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees (celsius) out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow +'. There is a ' + body.currently.precipProbability + '% chance of rain with a wind speed of ' + body.daily.data[0].windSpeed + ' m/s.'
                 // summary: body.daily.data[0].summary,
                 // temperature: body.currently.temperature,
                 // precipProbability: body.currently.precipProbability)
